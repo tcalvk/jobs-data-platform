a1bb80cb9847204c71b52c12493b3ee7f94881cc
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-03-16 21:36:09 -0600

    fixed dbt config and such

--- dbt/models/staging/jobs_scraping/stg_seeds__serpapi_query_versions.sql
+++ dbt/models/staging/jobs_scraping/stg_seeds__serpapi_query_versions.sql
@@ -8,8 +8,8 @@ with source as (
         max_jobs,
         google_domain,
         active,
-        q_plain_text
-    from {{ source('ao', 'serpapi_query_versions') }}
+        INITCAP(REPLACE(q, '+', ' ')) as q_plain_text
+    from {{ ref('serpapi_query_versions') }}
 )
 
 select * from source
