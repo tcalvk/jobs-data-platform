6ec2edd62bf4ea6b6e2521f0778120ce07290a65
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-03-20 23:48:53 -0600

    moved transactions to es

--- dbt/models/staging/personal_finance/stg_personal_finance__plaid_transactions.sql
+++ dbt/models/staging/personal_finance/stg_personal_finance__plaid_transactions.sql
@@ -1,23 +1,49 @@
 with source as (
     
     select 
-        date,
-        plaid_amount,
-        business,
-        plaid_category,
-        transaction_id,
-        account,
-        status,
-        coalesce(category_id_override, category_id_plaid) as category_id,
-        amount_override,
-        category,
-        amount,
-        transaction_type,
-        category_id_override,
-        category_id_plaid,
-        'plaid_transactions' as source
+        Date,
+        _Amount_,
+        Business,
+        Category,
+        TransactionID,
+        Account,
+        Status,
+        Category_Id__BSA_,
+        Last_Month,
+        To_Review,
+        Reviewed,
+        Category_Id_Override,
+        Amount_Override,
+        Category_Override,
+        Category__final__,
+        Amount__final_,
+        Month,
+        Transaction_Type
     from {{ source('personal_finance', 'plaid_transactions') }}
+
+),
+
+renamed as (
+
+    select 
+        Date as date,
+        _Amount_ as plaid_amount,
+        Business as business,
+        Category as plaid_category,
+        TransactionID as transaction_id,
+        Account as account,
+        Status as status,
+        coalesce(Category_Id_Override, Category_Id__BSA_) as category_id,
+        Amount_Override as amount_override,
+        Category__final__ as category,
+        Amount__final_ as amount,
+        Transaction_Type as transaction_type,
+        Category_Id_Override as category_id_override,
+        Category_Id__BSA_ as category_id_plaid,
+        'plaid_transactions' as source
+    from source
+    where Date is not null -- Remove blank rows in the sheet 
 
 )
 
-select * from source
+select * from renamed
