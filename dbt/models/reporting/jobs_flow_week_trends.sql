e2e445dcecb48c71f28509ef3687f9be2eaca46c
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-05-04 13:14:58 -0600

    fixed jobs flow week trends agg

--- dbt/models/reporting/jobs_flow_week_trends.sql
+++ dbt/models/reporting/jobs_flow_week_trends.sql
@@ -4,6 +4,17 @@ with new_jobs as (
     where type = 'New Jobs'
 )
 
+, agg as (
+    select 
+        week_beginning,
+        search_term,
+        search_location,
+        type,
+        sum(jobs_count) as jobs_count
+    from new_jobs
+    group by 1, 2, 3, 4
+)
+
 select *,
     (jobs_count - previous_week_jobs_count) / nullif(previous_week_jobs_count, 0) * 100 as wow_percent_change
 from (
@@ -12,5 +23,5 @@ from (
             partition by search_term, search_location 
             order by week_beginning asc 
         ) as previous_week_jobs_count
-    from new_jobs 
+    from agg 
 )
