a81a66b86051cb172e1d4c98db7b1e8612700365
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-05-05 19:29:55 -0600

    fixed fact and dim models to be more accurate

--- dbt/models/analysis/jobs_detail_day.sql
+++ dbt/models/analysis/jobs_detail_day.sql
@@ -2,17 +2,34 @@ with job_facts as (
     select * from {{ ref('fact_scraped_jobs') }}
 )
 
-, jobs_detail as (
-    select * from {{ ref('jobs_detail') }}
+, derived as (
+    select * except (job_description, search_location),
+        case 
+            when lower(job_title) like '%principal%' then 'Principal'
+            when lower(job_title) like '%distinguished%' then 'Principal'
+            when lower(job_title) like '%lead%' then 'Lead'
+            when lower(job_title) like '%sr%' then 'Senior'
+            when lower(job_title) like '%senior%' then 'Senior'
+            when lower(job_title) like '%mid%' then 'Mid Level'
+            when lower(job_title) like '%staff%' then 'Staff'
+            when lower(job_title) like '% iii' then 'Mid Level'
+            when lower(job_title) like '% ii' then 'Mid Level'
+            when lower(job_title) like '% 3' then 'Mid Level'
+            when lower(job_title) like '%entry%' then 'Entry'
+            when lower(job_title) like '%junior%' then 'Entry'
+            else 'Entry'
+        end as job_level,
+        initcap(search_location) as search_location,
+        -- This col's logic takes care of averaging if the low or high pay range is null (to ensure a true avg)
+        (COALESCE(low_annual_pay_range, 0) + COALESCE(high_annual_pay_range, 0)) / 
+        (
+            CASE 
+                WHEN low_annual_pay_range IS NULL AND high_annual_pay_range IS NULL THEN NULL 
+                ELSE (CASE WHEN low_annual_pay_range IS NULL THEN 0 ELSE 1 END) + 
+                    (CASE WHEN high_annual_pay_range IS NULL THEN 0 ELSE 1 END)
+            END
+        ) AS avg_annual_pay_range
+    from job_facts
 )
 
-, joined as (
-    select 
-        jf.created_at_utc,
-        jf.search_term,
-        jf.job_platform,
-        
-    from job_facts jf 
-    left join jobs_detail jd 
-        using (platform_job_id, company_name, data_source)
-)
+select * from derived
