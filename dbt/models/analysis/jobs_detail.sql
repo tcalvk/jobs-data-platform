a130d0cff20bd836de86cd1325b0b59c06ad4679
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-05-24 22:08:27 -0600

    some fixes

--- dbt/models/analysis/jobs_detail.sql
+++ dbt/models/analysis/jobs_detail.sql
@@ -2,33 +2,36 @@ with src as (
     select * from {{ ref('dim_job_listings') }}
 )
 
-, detail as (
+, detail_day as (
     select * from {{ ref('jobs_detail_day') }}
 )
 
-, _join as (
-    select 
-        s.* except (job_description, search_location),
-        d.job_level,
-        d.search_location,
-        avg(d.avg_annual_pay_range) as avg_annual_pay_range
+, _agg as (
+    select
+        job_id,
+        avg(avg_annual_pay_range) as avg_annual_pay_range,
+        max(created_at_utc) as last_seen_at_utc
+    from detail_day
+    group by 1
+)
+, joined as (
+    select
+        s.* except (low_annual_pay_range, high_annual_pay_range),
+        a.avg_annual_pay_range,
+        a.last_seen_at_utc
     from src s
-    left join detail d 
+    left join _agg a 
         using (job_id)
-    qualify row_number() over(
-        partition by d.job_id
-        order by d.created_at_utc asc 
-    )
 )
 
 , derive_removed_date as (
     select *,
         if(
             date_diff(date(current_timestamp()), date(last_seen_at_utc), day) >= 21,
-             date_add(date(last_seen_at_utc), interval 1 day),
-             cast(null as date)
+            date_add(date(last_seen_at_utc), interval 1 day),
+            cast(null as date)
         ) as removed_date
-    from src 
+    from joined 
 )
 
 , add_listing_info as (
--- dbt/models/analysis/search_term_opportunity_score_month.sql
+++ dbt/models/analysis/search_term_opportunity_score_month.sql
@@ -19,15 +19,16 @@ with fact_detail as (
     select
         fd.search_term,
         fd.search_location,
-        date_trunc(fd.posted_date, month) as month,
+        date_trunc(fd.posted_date, month) as month, -- structure opportunity scoring by month, so that it's not oversensitive, but still adjusts over time
         count(distinct fd.job_id) as active_jobs_count
-    from {{ ref('fact_detail') }} fd
+    from fact_detail fd
     left join dim_detail dd 
         using (job_id) 
     where dd.listing_status = 'Active'
     group by 1, 2, 3
 )
-, _window as (
+
+, _agg as (
     select
         month,
         search_term,
@@ -36,7 +37,7 @@ with fact_detail as (
         min(active_jobs_count) over () as min_active_jobs_count,
         max(active_jobs_count) over () as max_active_jobs_count
     from search_term_months
-    group by 1, 2, 3
+    group by 1, 2, 3, 4
 )
 , demand_score as (
     select
@@ -51,7 +52,7 @@ with fact_detail as (
                 max_active_jobs_count - min_active_jobs_count
             ) * 100
         end as demand_score
-    from _window
+    from _agg
 )
 
 select *
