8c6434832cc77279e1d0d31a119a06a240fcb08a
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-06-12 19:18:31 -0600

    added new jobs skills report

---
+++ dbt/models/reporting/rpt_job_skills.sql
@@ -0,0 +1,42 @@
+with jobs_detail as (
+    select * from {{ ref('jobs_detail') }}
+)
+
+, skills as (
+    select * from {{ ref('format_skills') }}
+)
+
+, search_term_opportunity_score_month as (
+    select
+
+        month_start_date,
+        search_term,
+        search_location,
+        opportunity_score,
+        opportunity_tier
+
+    from {{ ref('search_term_opportunity_score_month') }}
+)
+
+, joined as (
+
+    select
+        jd.*,
+        st.opportunity_score,
+        st.opportunity_tier,
+        s.skill
+    from jobs_detail jd 
+    left join search_term_opportunity_score_month st
+        on date_trunc(jd.posted_date, month) = st.month_start_date
+        and jd.search_term = st.search_term
+        and jd.search_location = st.search_location
+    left join skills s
+        on jd.job_id = s.job_id and jd.data_source = s.data_source
+        
+)
+
+select * from joined
+
+
+
+
