af329b85cb7c94788e788b64bd06f4b286aed3b9
tcalvk <103088458+tcalvk@users.noreply.github.com>
2026-07-24 18:11:26 -0600

    edited existing tests

--- dbt/tests/assert_search_term_opportunity_score_month_score_ranges.sql
+++
@@ -1,7 +0,0 @@
-select *
-from {{ ref('search_term_opportunity_score_month') }}
-where opportunity_score not between 0 and 100
-    or demand_score not between 0 and 100
-    or growth_score not between 0 and 100
-    or salary_score not between 0 and 100
-    or long_tail_score not between 0 and 100
